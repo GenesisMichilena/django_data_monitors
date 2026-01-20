538793461718d83229f95cabf8bef54fb02ca220
ManuelMatute <149851692+ManuelMatute@users.noreply.github.com>
2026-01-20 00:45:47 +0000

    railway start cambios

--- railway_start.sh
+++ railway_start.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+python -c "import os; k=['MYSQL_URL','MYSQLDATABASE','MYSQLUSER','MYSQLPASSWORD','MYSQLHOST','MYSQLPORT']; print({i: ('present' if os.getenv(i) else 'missing') for i in k})"
+
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-python manage.py shell -c "import os, sys; from django.contrib.auth import get_user_model; User=get_user_model(); u=os.getenv('DJANGO_SUPERUSER_USERNAME'); p=os.getenv('DJANGO_SUPERUSER_PASSWORD'); e=os.getenv('DJANGO_SUPERUSER_EMAIL'); 
-( u and p and e ) or sys.exit(0); 
+python manage.py shell -c "import os, sys; from django.contrib.auth import get_user_model; User=get_user_model(); u=os.getenv('DJANGO_SUPERUSER_USERNAME'); p=os.getenv('DJANGO_SUPERUSER_PASSWORD'); e=os.getenv('DJANGO_SUPERUSER_EMAIL');
+( u and p and e ) or sys.exit(0);
 User.objects.filter(username=u).exists() or User.objects.create_superuser(u,e,p)"
 
-exec gunicorn backend_analytics_server.wsgi:application --bind 0.0.0.0:${PORT}
+exec gunicorn backend_analytics_server.wsgi:application --bind 0.0.0.0:$PORT
